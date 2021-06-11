460e4145e41f581f11cb8491482f21a07e5a26f8
Juliana <juliana.mohamedali@gmail.com>
2021-06-11 18:13:20 +0800

    remove verify in answer

--- answers/models.py
+++ answers/models.py
@@ -19,8 +19,6 @@ class Answer(models.Model):
     question = models.ForeignKey(
         Question, on_delete=models.CASCADE, related_name='answers_qn')
 
-    verify = models.ManyToManyField(User, related_name='ans_post')
-
     def serialize(self):
         return {
             "id": self.id,
